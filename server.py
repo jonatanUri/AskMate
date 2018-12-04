2c705fe61eda4396eb32583346744996341a577b
Jonatan Uri <urijoni@gmail.com>
2018-12-04 17:18:40 +0100

    I just saved your asses working overtime lmao

--- server.py
+++ server.py
@@ -9,5 +9,11 @@ def mainpage():
     return render_template("index.html", questions_dict=questions_dict)
 
 
+@app.route('/question/<num>')
+def route_save_edited(num):
+    questions_dict = data_manager.csv_read_question()
+    return render_template("question.html", num=num, questions_dict=questions_dict)
+
+
 if __name__ == "__main__":
     app.run()

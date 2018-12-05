00da9510b7a7e4c353db0046c89c8f5d5f43420c
Jonatan Uri <urijoni@gmail.com>
2018-12-05 13:02:43 +0100

    Added useful backend functions

--- data_manager.py
+++ data_manager.py
@@ -1,10 +1,59 @@
 import csv
+import time
+
+question_path="/home/jonatan/codecool/WEB/SI1/ksfhdjgfh/sample_data/question.csv"
+answer_path="/home/jonatan/codecool/WEB/SI1/ksfhdjgfh/sample_data/answer.csv"
 
 
-def csv_read_question():
-    with open("/home/jonatan/codecool/WEB/SI1/ksfhdjgfh/sample_data/question.csv", "r") as questions:
+def read_question():
+    global question_path
+    return read_csv(question_path)
+
+def read_answer():
+    global answer_path
+    return  read_csv(answer_path)
+
+
+def read_csv(path):
+    with open(path, "r") as questions:
         reader = csv.DictReader(questions)
         myList = []
         for row in reader:
+            if 'submission_time' in row:
+                row['submission_time'] = convert_time(row['submission_time'])
             myList.append(row)
     return myList
+
+
+def convert_time(unix_timestamp):
+    readable_time = time.ctime(int(unix_timestamp))
+    return readable_time
+
+
+def delete_answer(id_):
+    answers = read_answer()
+    for answer in answers:
+        if id_ == answer['id']:
+            answers.remove(answer)
+    rewrite_csv(answers,answer_path)
+
+
+def delete_question(id_):
+    questions = read_question()
+    answers = read_answer()
+    for question in questions:
+        if id_ == question['id']:
+            questions.remove(question)
+    rewrite_csv(questions,question_path)
+    for answer in answers:
+        if id_ == answer['question_id']:
+            answers.remove(answer)
+    rewrite_csv(answers, answer_path)
+
+
+def rewrite_csv(data, path):
+    with open(path, "w") as csvfile:
+        writer = csv.DictWriter(csvfile)
+        writer.writeheader()
+        for i in data:
+            writer.writerow(i)
--- server.py
+++ server.py
@@ -3,15 +3,16 @@ import data_manager
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def mainpage():
-    questions_dict = data_manager.csv_read_question()
+    questions_dict = data_manager.read_question()
     return render_template("index.html", questions_dict=questions_dict)
 
 
 @app.route('/question/<num>')
 def route_save_edited(num):
-    questions_dict = data_manager.csv_read_question()
+    questions_dict = data_manager.read_question()
     return render_template("question.html", num=num, questions_dict=questions_dict)
 
 

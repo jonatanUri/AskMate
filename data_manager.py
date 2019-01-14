b71c508367420dc23870bad9f815a549caf45747
Jonatan Uri <urijoni@gmail.com>
2019-01-14 10:47:45 +0100

    sql sample data

--- data_manager.py
+++ data_manager.py
@@ -4,6 +4,8 @@ import data_paths
 
 question_path = data_paths.question_path
 answer_path = data_paths.answer_path
+question_header = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
+answer_header = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
 
 
 def read_question():
@@ -13,7 +15,7 @@ def read_question():
 
 def read_answer():
     global answer_path
-    return  read_csv(answer_path)
+    return read_csv(answer_path)
 
 
 def read_csv(path):
@@ -21,10 +23,17 @@ def read_csv(path):
         reader = csv.DictReader(questions)
         myList = []
         for row in reader:
-            if 'submission_time' in row:
-                row['submission_time'] = convert_time(row['submission_time'])
             myList.append(row)
     return myList
+
+
+def convert_csv_to_human_readable(list_of_dicts):
+    new_csv = []
+    for row in list_of_dicts:
+        if 'submission_time' in row:
+            row['submission_time'] = convert_time(row['submission_time'])
+        new_csv.append(row)
+    return new_csv
 
 
 def convert_time(unix_timestamp):
@@ -37,7 +46,7 @@ def delete_answer(id_):
     for answer in answers:
         if id_ == answer['id']:
             answers.remove(answer)
-    rewrite_csv(answers,answer_path)
+    rewrite_csv(answers, answer_path, answer_header)
 
 
 def delete_question(id_):
@@ -46,30 +55,65 @@ def delete_question(id_):
     for question in questions:
         if id_ == question['id']:
             questions.remove(question)
-    rewrite_csv(questions,question_path)
+            break
+    rewrite_csv(questions, question_path, question_header)
     for answer in answers:
         if id_ == answer['question_id']:
             answers.remove(answer)
-    rewrite_csv(answers, answer_path)
+    rewrite_csv(answers, answer_path, answer_header)
 
 
-def rewrite_csv(data, path):
+def rewrite_csv(data, path, header):
     with open(path, 'w') as csvfile:
-        writer = csv.DictWriter(csvfile)
+        writer = csv.DictWriter(csvfile, header)
         writer.writeheader()
         for i in data:
             writer.writerow(i)
 
 
-def add_question():
-    questions=read_question()
+def add_question(new_question):
     with open(question_path, 'a') as csvfile:
-        writer = csv.DictWriter(csvfile)
-        writer.writerow()
+        writer = csv.DictWriter(csvfile, question_header)
+        writer.writerow(new_question)
 
 
-def add_answer():
-    answers=read_answer()
+def add_answer(new_answer):
     with open(answer_path, 'a') as csvfile:
-        writer = csv.DictWriter(csvfile)
-        writer.writerow()
+        writer = csv.DictWriter(csvfile, answer_header)
+        writer.writerow(new_answer)
+
+
+def sorted_by_submission_time(list_of_dicts):
+    n = len(list_of_dicts)
+    for i in range(n):
+        for j in range(i, n):
+            if list_of_dicts[j]['submission_time'] > list_of_dicts[i]['submission_time']:
+                temp = list_of_dicts[i]
+                list_of_dicts[i] = list_of_dicts[j]
+                list_of_dicts[j] = temp
+    return list_of_dicts
+
+
+def get_new_question_id():
+    questions = read_question()
+    max_id = "0"
+    for i in questions:
+        if max_id < i['id']:
+            max_id = i['id']
+    max_id = int(max_id) + 1
+    return str(max_id)
+
+
+def get_new_answer_id():
+    answers = read_answer()
+    max_id = "0"
+    for i in answers:
+        if max_id < i['id']:
+            max_id = i['id']
+    max_id = int(max_id) + 1
+    return str(max_id)
+
+
+def get_current_unix_timestamp():
+    current_time = time.time()
+    return int(current_time)
--- server.py
+++ server.py
@@ -10,33 +10,96 @@ def workinprogress():
 
 
 @app.route('/')
+def home_redirect():
+    return redirect('/list')
+
+
+@app.route('/list')
 def mainpage():
     questions_dict = data_manager.read_question()
+    questions_dict = data_manager.sorted_by_submission_time(questions_dict)
     return render_template("index.html", questions_dict=questions_dict)
 
 
 @app.route('/question/<num>')
 def question(num):
     questions_dict = data_manager.read_question()
-    answer_dict = data_manager.read_answer()
-    return render_template("question.html", num=num, questions_dict=questions_dict, answer_dict=answer_dict)
+    questions_dict = data_manager.convert_csv_to_human_readable(questions_dict)
+    answers_dict = data_manager.read_answer()
+    answers_dict = data_manager.convert_csv_to_human_readable(answers_dict)
+    for i in questions_dict:
+        if i['id'] == num:
+            this_question = i
+            break
+    answers_list = []
+    for answer in answers_dict:
+        if num == answer["question_id"]:
+            answers_list.append(answer)
+    return render_template("question.html", num=num, this_question=this_question, answers_list=answers_list)
 
 
-@app.route('/question/<num>/answer')
-def add_answer(num):
-    questions_dict = data_manager.read_question()
-    answers_dict = data_manager.read_csv()
-    return render_template("new-answer.html", num=num, questions_dict=questions_dict, answer_dicts=answers_dict)
+@app.route('/question/<num>/new-answer')
+def new_answer(num):
+    return render_template("new-answer.html", num=num)
 
 
 @app.route('/new-question')
 def new_question():
-    return None
+    return render_template("new-question.html")
+
+
+@app.route('/submit-question', methods=['GET', 'POST'])
+def submit_question():
+    if request.method == 'POST':
+        id_ = data_manager.get_new_question_id()
+        submission_time = data_manager.get_current_unix_timestamp()
+        title = request.form['title']
+        message = request.form['message']
+        views = 0
+        votes = 0
+        question_dict = {
+            'id': id_,
+            'submission_time': submission_time,
+            'view_number': views,
+            'vote_number': votes,
+            'title': title,
+            'message': message
+        }
+        data_manager.add_question(question_dict)
+    return redirect('/question/'+id_)
+
+
+@app.route('/submit-answer', methods=['GET', 'POST'])
+def submit_answer():
+    if request.method == 'POST':
+        id_ = data_manager.get_new_answer_id()
+        submission_time = data_manager.get_current_unix_timestamp()
+        votes = 0
+        question_id = request.form['question_id']
+        message = request.form['message']
+        answer_dict = {
+            'id': id_,
+            'submission_time': submission_time,
+            'vote_number': votes,
+            'question_id': question_id,
+            'message': message
+        }
+        data_manager.add_answer(answer_dict)
+    return redirect('/question/'+question_id)
+
+
+@app.route('/delete-question/<num>')
+def delete_question(num):
+    data_manager.delete_question(num)
+    return redirect('/list')
+
+
+@app.route('/answer/<num>/delete', methods=['POST'])
+def delete_answer(num):
+    data_manager.delete_answer(request.form['answer_id'])
+    return redirect('/question/'+num)
 
 
 if __name__ == "__main__":
     app.run()
 
-
-# request.form['name'] in py, and name in html
-# <form method = "post">
